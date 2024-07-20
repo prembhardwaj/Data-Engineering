147ab94d9183cf33a37c41f81220460b098f3019
prembhardwaj <prem_bhardwaj@yahoo.com>
2024-07-19 22:17:54 -0400

    first commit

---
+++ Module 9/test.py
@@ -0,0 +1,28 @@
+     
+def def_calculator(x,y,o):
+    if o == "+":
+        cal = x + y
+    elif o == "-":
+        cal = x - y
+    elif o == "*":
+        cal = x*y
+    elif cal == "/":
+        cal = x*y
+    else:
+        print( "Operator not recognized")
+
+    return cal
+
+
+try :
+    x = int(input("Enter X: "))
+    y = int(input("Enter Y: "))
+    o = input("Enter operator: ")
+    
+except (TypeError, ValueError):
+    print("check input values for X & Y should be integers")
+
+else:
+    result = def_calculator(x,y,o)
+    print(result)
+
